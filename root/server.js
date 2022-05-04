9173bb7be97649948f5cc2d8f03bc1c019ff0f84
Purple-Towel <angadgrewal31@gmail.com>
2022-05-04 12:40:46 -0700

    commented out tinyeditor from server skeleton as it requires HTML documents yet to be written

--- root/server.js
+++ root/server.js
@@ -5,6 +5,8 @@ const jsdom = require("jsdom");
 const mysql2 = require("mysql2");
 const http = require("http");
 const https = require("https");
-const tinyedit = require("tiny-edit");
+// tiny-editor requires that there be a document object from HTML; 
+// as there is no HTML element just yet, it is safe to comment out
+// const tinyeditor = require("tiny-editor"); 
 const sanitize = require("sanitize-html");
 const multer = require("multer");
