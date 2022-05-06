8d85c1f964a3677cd7f61677c55a76de3eb31986
Kelvin Zhou <szhou54bcit@gmail.com>
2022-05-06 05:28:02 -0700

    still working on it...

--- root/server.js
+++ root/server.js
@@ -20,6 +20,36 @@ const multer = require("multer");
 
 const app = express();
 
+// Oh look, unsecured data that will be moved to an .env at some point in future
+// and no; we probably won't use this exact data again.
+const dbConnection = {
+  host: "localhost",
+  user: "nodeapp",
+  password: "",
+  database: "db_petpals",
+  port: 3306
+};
+const mysql2 = require("mysql2");
+const connection = mysql2.createConnection(dbConnection);
+connection.connect((err) => {
+  if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+  };
+
+  console.log("connected successfully");
+});
+
+
+// initializing sessions
+let sessionObj = {
+  secret: "Hey, another password that will be obscured eventually. Neat!",
+  name: "petpalsID",
+  resave: false,
+  saveUninitialized: true
+};
+
+app.use(session(sessionObj));
 
 app.use("/common", express.static("./root/common"));
 app.use("/script", express.static("./root/script"));
@@ -30,7 +60,7 @@ app.use("/js", express.static("./root/js"));
 app.use("/scss", express.static("./root/scss"));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
 
 app.post('/add-account', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
@@ -56,83 +86,51 @@ app.post('/add-account', function (req, res) {
 });
 
 
-
-// Oh look, unsecured data that will be moved to an .env at some point in future
-// and no; we probably won't use this exact data again.
-const dbConnection = {
-    host: "localhost",
-    user: "nodeapp",
-    password: "",
-    database: "db_petpals",
-    port: 3306
-};
-const mysql2 = require("mysql2");
-const connection = mysql2.createConnection(dbConnection);
-connection.connect((err) => {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    };
-
-    console.log("connected successfully");
-});
-
-
-// initializing sessions
-let sessionObj = {
-    secret: "Hey, another password that will be obscured eventually. Neat!",
-    name: "petpalsID",
-    resave: false,
-    saveUninitialized: true
-};
-
-app.use(session(sessionObj));
-
 app.get("/", (req, res) => {
-    res.redirect("/home");
+  res.redirect("/home");
 });
 
 app.get("/home", (req, res) => {
-    if (!(req.session.loggedIn)) {
-        res.redirect("/login");
-    } else {
-        let doc = fs.readFileSync("./root/index.html", "utf-8");
-        res.send(doc);
-    }
+  if (!(req.session.loggedIn)) {
+      res.redirect("/login");
+  } else {
+      let doc = fs.readFileSync("./root/index.html", "utf-8");
+      res.send(doc);
+  }
 });
 
 app.get("/login", (req, res) => {
-    let doc = fs.readFileSync("./root/login.html", "utf-8");
-    res.send(doc);
+  let doc = fs.readFileSync("./root/login.html", "utf-8");
+  res.send(doc);
 });
 
 app.post("/login", jsonParser, (req, res) => {
-    res.setHeader("content-type", "application/json");
-    console.log(req);
-    let username = req.body.username;
-    let password = req.body.password;
-    if (username && password) {
-        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], (err, data, fields) => {
-            if (err) throw err;
-            if (data.length > 0) {
-                req.session.loggedIn = true;
-                req.session.username = data[0].username;
-                req.session.name = data[0].firstname + " " + data[0].lastname;
-                req.session.username = data[0].username;
-                req.session.userid = data[0].id;
-                req.session.admin = data[0].is_admin;
-                req.session.caretaker = data[0].is_caretaker;
-                req.session.save((e) => {
-                    if (e) {
-                        console.log("Error: " +e);
-                    }
-                });
-                res.send({status: "success", msg: "Log In Successful"});
-            } else {
-                res.send({status: "failure", msg: "Log In Unsuccessful"});
-            }
-        });
-    }
+  res.setHeader("content-type", "application/json");
+  console.log(req);
+  let username = req.body.username;
+  let password = req.body.password;
+  if (username && password) {
+      connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], (err, data, fields) => {
+          if (err) throw err;
+          if (data.length > 0) {
+              req.session.loggedIn = true;
+              req.session.username = data[0].username;
+              req.session.name = data[0].firstname + " " + data[0].lastname;
+              req.session.username = data[0].username;
+              req.session.userid = data[0].id;
+              req.session.admin = data[0].is_admin;
+              req.session.caretaker = data[0].is_caretaker;
+              req.session.save((e) => {
+                  if (e) {
+                      console.log("Error: " +e);
+                  }
+              });
+              res.send({status: "success", msg: "Log In Successful"});
+          } else {
+              res.send({status: "failure", msg: "Log In Unsuccessful"});
+          }
+      });
+  }
 });
 
 console.log("Starting Server...");
