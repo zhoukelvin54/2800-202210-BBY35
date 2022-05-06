9468a9c4ea2a1462d5e076fae9359a324ebe527a
Kelvin Zhou <szhou54bcit@gmail.com>
2022-05-05 18:04:49 -0700

    added hero image and working on landing page content

---
+++ root/script/index.js
@@ -0,0 +1,6 @@
+function hidehero() {
+    document.getElementById("welcome-img").style.display="none";
+    document.getElementById("content-img").style.display="flex";
+    console.log("clicked");
+    
+};
