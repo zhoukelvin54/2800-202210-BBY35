b94a2f4e0d35eaded0f9eb6ade55b377fe76d156
Dakaro Mueller <LukarioMC@gmail.com>
2022-05-06 03:04:59 -0700

    Created functions to get users data

--- root/js/user_management.js
+++ root/js/user_management.js
@@ -1,20 +1,34 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   console.log("User management script loaded.");
+  let table = document.getElementById("userTable").querySelector("tbody");
 
-  let userData = getUserData();
+  let userData = await getUserData()
+  userData.forEach(user => {
+    let row = document.createElement("tr");
+    let tableFields = ["username", "firstname", "lastname", "email", "is_admin", "is_caretaker"];
+    
+    tableFields.forEach(field => {
+      let currentField = document.createElement("td");
+      currentField.innerText = user[field];
+      row.appendChild(currentField);
+    });
+
+    table.appendChild(row);
+  });
 });
 
 async function getUserData() {
   try {
     let response = await fetch("/userData", {
       method: 'GET',
-      body: "SessionID" // TODO Insert actual session ID to verify current logged in user is in fact Admin
     });
 
-    if (response.status = 200) {
-      return JSON.parse(await response.text());
+    if (response.status == 200) {
+      let data = await response.text();
+      // console.log(data);
+      return JSON.parse(data);
     } else {
       console.log(response.status);
       console.log(response.statusText);
