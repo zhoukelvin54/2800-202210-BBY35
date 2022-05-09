0d83b819bcb083c3ac708d614809c6a7958a218a
Purple-Towel <angadgrewal31@gmail.com>
2022-05-09 16:33:00 -0700

    changed port to 8000

--- root/server.js
+++ root/server.js
@@ -179,7 +179,7 @@ app.get("/userData", (req, res) => {
 
 console.log("Starting Server...");
 
-const port = 8001;
+const port = 8000;
 function onBoot() {
     console.log("Started on port: " + port);
 };
