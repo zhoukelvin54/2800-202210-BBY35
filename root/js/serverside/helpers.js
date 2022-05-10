1016639a37853d8cdb4f579b0936e97672962d96
Purple-Towel <angadgrewal31@gmail.com>
2022-05-09 20:53:22 -0700

    made sure to properly separate client and serverside scripts

---
+++ root/js/serverside/helpers.js
@@ -0,0 +1,3 @@
+// this file is for helper functions to keep our server code clean.
+// also, git doesn't like empty folders so this will also server to make sure this subdirectory
+// isn't skipped over.
