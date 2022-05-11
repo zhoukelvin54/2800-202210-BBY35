790d0056fc4469c8f95d4e1e21c4924c2592c3ba
Purple-Towel <angadgrewal31@gmail.com>
2022-05-11 15:44:28 -0700

    fixed typo in sql causing certain lines to never execute

--- schemas/3_db_accounts_insert.sql
+++ schemas/3_db_accounts_insert.sql
@@ -17,44 +17,44 @@ email, password, is_admin, is_caretaker) VALUES (
 INSERT INTO BBY35_accounts (id, username, firstname, lastname,
 email, password, is_admin, is_caretaker) VALUES (
 5, 'admin1', 'Ad.', 'Min.', 'admin@email.com', '12345', 1, 0
-)
+);
 
 INSERT INTO BBY35_accounts (id, username, firstname, lastname,
 email, password, is_admin, is_caretaker) VALUES (
 6, 'admin2', 'Ad.', 'Min.', 'admin@email.com', '12345', 1, 0
-)
+);
 
 INSERT INTO BBY35_accounts (id, username, firstname, lastname,
 email, password, is_admin, is_caretaker) VALUES (
 7, 'admin3', 'Ad.', 'Min.', 'admin@email.com', '12345', 1, 0
-)
+);
 
 INSERT INTO BBY35_accounts (id, username, firstname, lastname,
 email, password, is_admin, is_caretaker) VALUES (
 8, 'caretaker1', 'Care', 'Taker', 'caretaker@email.com', '12345', 0, 1
-)
+);
 
 INSERT INTO BBY35_accounts (id, username, firstname, lastname,
 email, password, is_admin, is_caretaker) VALUES (
 9, 'caretaker2', 'Care', 'Taker', 'caretaker@email.com', '12345', 0, 1
-)
+);
 
 INSERT INTO BBY35_accounts (id, username, firstname, lastname,
 email, password, is_admin, is_caretaker) VALUES (
 10, 'caretaker3', 'Care', 'Taker', 'caretaker@email.com', '12345', 0, 1
-)
+);
 
 INSERT INTO BBY35_accounts (id, username, firstname, lastname,
 email, password, is_admin, is_caretaker) VALUES (
 11, 'user1', 'End', 'User', 'user@email.com', '12345', 0, 0
-)
+);
 
 INSERT INTO BBY35_accounts (id, username, firstname, lastname,
 email, password, is_admin, is_caretaker) VALUES (
 12, 'user2', 'End', 'User', 'user@email.com', '12345', 0, 0
-)
+);
 
 INSERT INTO BBY35_accounts (id, username, firstname, lastname,
 email, password, is_admin, is_caretaker) VALUES (
 13, 'user3', 'End', 'User', 'user@email.com', '12345', 0, 0
-)
+);
