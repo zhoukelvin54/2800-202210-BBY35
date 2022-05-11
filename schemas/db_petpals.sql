d68a780f5f73e0206dd153bae91152a802bc813b
Purple-Towel <angadgrewal31@gmail.com>
2022-05-11 12:35:36 -0700

    modified schemas to use the required namespacing of COMP2537

--- schemas/db_petpals.sql
+++ schemas/db_petpals.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `db_petpals` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `db_petpals`;
+CREATE DATABASE  IF NOT EXISTS `COMP2800` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `COMP2800`;
 -- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
 --
--- Host: localhost    Database: db_petpals
+-- Host: localhost    Database: COMP2800
 -- ------------------------------------------------------
 -- Server version	8.0.28
 
@@ -18,13 +18,13 @@ USE `db_petpals`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `accounts`
+-- Table structure for table `BBY35_accounts`
 --
 
-DROP TABLE IF EXISTS `accounts`;
+DROP TABLE IF EXISTS `BBY35_accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8 */;
-CREATE TABLE `accounts` (
+CREATE TABLE `BBY35_accounts` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `firstname` varchar(45) DEFAULT NULL,
@@ -38,13 +38,13 @@ CREATE TABLE `accounts` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `pets`
+-- Table structure for table `BBY35_pets`
 --
 
-DROP TABLE IF EXISTS `pets`;
+DROP TABLE IF EXISTS `BBY35_pets`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8 */;
-CREATE TABLE `pets` (
+CREATE TABLE `BBY35_pets` (
   `id` int NOT NULL AUTO_INCREMENT,
   `owner_id` int NOT NULL,
   `caretaker_id` int DEFAULT NULL,
@@ -56,8 +56,8 @@ CREATE TABLE `pets` (
   PRIMARY KEY (`id`),
   KEY `owner_id_idx` (`owner_id`),
   KEY `caretaker_id_idx` (`caretaker_id`),
-  CONSTRAINT `caretaker_id` FOREIGN KEY (`caretaker_id`) REFERENCES `accounts` (`id`),
-  CONSTRAINT `owner_id` FOREIGN KEY (`owner_id`) REFERENCES `accounts` (`id`)
+  CONSTRAINT `caretaker_id` FOREIGN KEY (`caretaker_id`) REFERENCES `BBY35_accounts` (`id`),
+  CONSTRAINT `owner_id` FOREIGN KEY (`owner_id`) REFERENCES `BBY35_accounts` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
