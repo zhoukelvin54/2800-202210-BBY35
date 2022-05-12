e3ef60764d7d1cedf7f07f0a0a7751d33a44b81b
Dakaro Mueller <LukarioMC@gmail.com>
2022-05-12 16:43:57 -0700

    Created layout for the caretaker information table.

--- schemas/2_db_petpals_create.sql
+++ schemas/2_db_petpals_create.sql
@@ -40,3 +40,26 @@ CREATE TABLE `BBY35_pets` (
   CONSTRAINT `caretaker_id` FOREIGN KEY (`caretaker_id`) REFERENCES `BBY35_accounts` (`id`),
   CONSTRAINT `owner_id` FOREIGN KEY (`owner_id`) REFERENCES `BBY35_accounts` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Table structure for table `BBY_caretaker_info
+--
+
+DROP TABLE IF EXISTS `BBY_caretaker_info`;
+
+CREATE TABLE `BBY_caretaker_info` (
+  `account_id` int NOT NULL,
+  `animal_affection` int NOT NULL,
+  `experience` varchar(255) NOT NULL,
+  `allergies` varchar(255) DEFAULT NULL,
+  `other_pets` varchar(255) DEFAULT NULL,
+  `busy_hours` varchar(255) DEFAULT NULL,
+  `house_type` varchar(32) NOT NULL,
+  `house_active_level` int NOT NULL,
+  `people_in_home` int NOT NULL,
+  `children_in_home` boolean NOT NULL,
+  `yard_type` varchar(32) NOT NULL,
+  PRIMARY KEY (`account_id`),
+  key `account_id_idx` (`account_id`),
+  CONSTRAINT `account_id` FOREIGN KEY (`account_id`) REFERENCES `BBY35_accounts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
