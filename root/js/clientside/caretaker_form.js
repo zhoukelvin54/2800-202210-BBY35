d3c8fee5f1f7540f4b7ca3e3c064a2b00250388d
Dakaro Mueller <LukarioMC@gmail.com>
2022-05-12 15:21:38 -0700

    Converted caretaker form to use dot notation.

--- root/js/clientside/caretaker_form.js
+++ root/js/clientside/caretaker_form.js
@@ -1,28 +1,31 @@
+// used for validating the code with https://jshint.com/
+/* jshint esversion: 8 */
+/* jshint browser: true */
 "use strict";
 
 function submitCaretakerInfo() {
-  let form = document.forms["user-creation-form"];
+  let form = document.forms.user_creation_form;
   let profileInfo = {
-    profile_picture: form["upload-profile-picture"].files[0],
-    telephone: form["telephone"].value,
-    street_address: form["street-address"].value,
-    region: form["region"].value,
-    country: form["country"].value
-  }
+    profile_picture: form.upload_profile_picture.files[0],
+    telephone: form.telephone.value,
+    street_address: form.street_address.value,
+    region: form.region.value,
+    country: form.country.value
+  };
 
   let caretakerInfo = {
-    animal_affection: form["animal-affection"].value,
-    experience: form["experience"].value,
-    allergies: form["allergies"].value,
-    other_pets: form["other-pets"].value,
-    busy_hours: form["busy-hours"].value,
-    house_type: form["house-type"].value,
-    house_active_level: form["house-active-level"].value,
-    people_in_home: form["people-in-home"].value,
-    children_in_home: form["children-in-home"].value,
-    yard_type: form["yard-type"].value,
-    accomodation_picture: form["accomodation-picture"].files
-  }
+    animal_affection: form.animal_affection.value,
+    experience: form.experience.value,
+    allergies: form.allergies.value,
+    other_pets: form.other_pets.value,
+    busy_hours: form.busy_hours.value,
+    house_type: form.house_type.value,
+    house_active_level: form.house_active_level.value,
+    people_in_home: form.people_in_home.value,
+    children_in_home: form.children_in_home.value,
+    yard_type: form.yard_type.value,
+    accomodation_picture: form.accomodation_picture.files
+  };
 
   console.log(profileInfo, caretakerInfo);
 }
