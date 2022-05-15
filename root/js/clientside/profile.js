659806d2fa23f8e20b9d2815f6b11e7a2aab7cb0
Dakaro Mueller <LukarioMC@gmail.com>
2022-05-14 18:06:37 -0700

    Profile details now update, just working out bugs in getting the fields from the page.

--- root/js/clientside/profile.js
+++ root/js/clientside/profile.js
@@ -1,11 +1,13 @@
 /* jshint esversion: 8 */
 /* jshint browser: true */
 "use strict";
-let swappableElements = [document.getElementById("username"), document.getElementById("first_name"), document.getElementById("last_name"), document.getElementById("email")];
+let swappableElements = document.querySelectorAll(".editable");
 
 document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("save_profile").addEventListener("click", updateProfile);
+
   for (let i = 0; i < swappableElements.length; i++) {
-    swappableElements[i].addEventListener("click", e => {swapEditableSpan(e.target)});
+    swappableElements[i].addEventListener("click", e => {swapSpanToInput(e.target)});
   }
 });
 
@@ -20,20 +22,60 @@ function swapEditableSpan(element) {
 
 function swapSpanToInput(element) {
   if (!element) throw "Invalid param: " + element;
+  if(element.tagName != "SPAN") return;
+
   let input = document.createElement("input");
   input.value = element.textContent;
   input.classList = element.classList;
   input.id = element.id;
 
   element.parentNode.replaceChild(input, element);
+  swappableElements = document.querySelectorAll(".editable")
 }
 
 function swapInputToSpan(element) {
   if (!element) throw "Invalid param: " + element;
+  if (element.tagName != "INPUT") return;
+
   let span = document.createElement("span");
   span.textContent = element.value;
   span.classList = element.classList;
   span.id = element.id;
 
   element.parentNode.replaceChild(span, element);
+  swappableElements = document.querySelectorAll(".editable")
+}
+
+// ============================================================================
+// Sends profile information update request from the form.
+// ============================================================================
+async function updateProfile() {
+  await fetch("/update-profile", { 
+    method: "PUT",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(getProfileData())
+  }).then(
+    () => {
+      for (let i = 0; i < swappableElements.length; i++) {
+        swapInputToSpan(swappableElements[i]);
+      }
+    }
+  ).catch(err => {
+    throw err;
+  });
+}
+
+function getProfileData() {
+  document.querySelectorAll("input[type='text'], input[type='password']").forEach(element => {
+    console.log(element.id);
+  });
+  return {
+    username: document.getElementById("username").value.trim(),
+    firstname: document.getElementById("first_name").value.trim(),
+    lastname: document.getElementById("last_name").value.trim(),
+    email: document.getElementById("email").value.trim(),
+    password: document.getElementById("new_password").value.trim()
+  }
 }
