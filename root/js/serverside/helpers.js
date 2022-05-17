318d6b29941f91fdb1e119bc3aa7c87bb5a6a33d
Dakaro Mueller <LukarioMC@gmail.com>
2022-05-16 21:55:21 -0700

    Added and documented craftUpdateQueryFromRequest function.

--- root/js/serverside/helpers.js
+++ root/js/serverside/helpers.js
@@ -31,4 +31,63 @@ async function loadHTMLComponent(baseDOM, templateSelector, templateLocation) {
   return baseDOM;
 }
 
-export { injectHeaderFooter, loadHTMLComponent };
+/**
+ * Crafts an SQL query in the format of 
+ * "INSERT INTO table (`{key_column}`, `{Recieved fields}`, [...]) VALUES (?, [...])
+ * ON DUPLICATE KEY UPDATE {Recieved field}=VALUES(Recieved field), [...]"
+ * 
+ * @author Dakaro Mueller
+ * @param {Request} req Request object with a body containing the JSON values under the columns to update.
+ * @param {String[]}expectedFields Array of strings matching the expected updatable database columns.
+ * @param {String} table Table name to insert / update into.
+ * @param {String} key_column Identifying field column name from database.
+ * @param {*} key_value Value of key column to insert into or update.
+ * @throws Exception if any parameter is "truthy" false.
+ */
+ function craftInsertUpdateQueryFromRequest(req, expectedFields, table, key_column, key_value) {
+  if (!req || !expectedFields || !table || !key_column || !key_value) throw "Null or empty parameter!";
+  console.log(req.body);
+  
+  let query = `INSERT INTO \`${table}\` (\`${key_column}\`, \``;
+  
+  let fields = getFieldsFromRequest(req, expectedFields);
+  let fieldValues = fields.fieldValues;
+  let recievedFields = fields.recievedFields;
+
+  query += ") VALUES (?";
+  // Append the amount of question marks for the prepared statement from recieved fields
+  for (let i = 0; i < recievedFields.length; i++) query += ",?";
+  query += ") ON DUPLICATE KEY UPDATE ";
+
+  // Append actually recieved fields to update as {field_name}=VALUES({field_name})
+  for (let i = 0; i < recievedFields.length; i++) {
+      query += recievedFields[i] +"=VALUES(" + recievedFields[i] + ")";
+      query += (i == recievedFields.length - 1) ? ";" : ",";
+  }
+
+  console.log(query);
+  return query;
+}
+
+/**
+ * Parses a request for fields from the request body and formats it into a JSON object
+ * containing arrays of the fields and their data.
+ * @param {*} req 
+ * @param {*} expectedFields 
+ * @returns JSON object containing a recievedFields array and a fieldValues array.
+ */
+function getFieldsFromRequest(req, expectedFields) {
+  let recievedFields, fieldValues = [];
+
+  // Parse through the req.body for expected props and append like so "[...], `{prop}`[...]";
+  for (let prop in req.body) {
+    if (expectedFields.includes(prop)) {
+      recievedFields.push(prop);
+      query += ", `" + prop + "`";
+      fieldValues.push(req.body[prop]);
+    }
+  }
+  return { "recievedFields": recievedFields, "fieldValues": fieldValues }
+}
+
+export { injectHeaderFooter, loadHTMLComponent, craftInsertUpdateQueryFromRequest };
