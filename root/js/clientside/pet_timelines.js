03da5f23f8fb0f0cfc4fd5364edb30db8568f7a1
Dakaro Mueller <LukarioMC@gmail.com>
2022-05-18 14:48:40 -0700

    Created basic pet timeline js

---
+++ root/js/clientside/pet_timelines.js
@@ -0,0 +1,40 @@
+onReady(() => {
+  // Get the data for specific type of user?
+});
+
+/**
+ * Gets the pet data for the currently logged in pet owner.
+ */
+async function getOwnerPets() {
+  fetch("/petData", {
+    method: "GET"
+  }).then(response => {
+    if (response.status == 200) {
+      let data = await response.text();
+      return JSON.parse(data);
+    } else {
+      console.error(response.status, response.statusText)
+    }
+  }).catch(error => {
+    console.error(error);
+  });
+}
+
+/**
+ * Gets the pet data for the currently logged in caretaker.
+ * TODO: GET THE CARETAKER ENDPOINT
+ */
+ async function getCaretakerPets() {
+  fetch("/petData", {
+    method: "GET"
+  }).then(response => {
+    if (response.status == 200) {
+      let data = await response.text();
+      return JSON.parse(data);
+    } else {
+      console.error(response.status, response.statusText)
+    }
+  }).catch(error => {
+    console.error(error);
+  });
+}
