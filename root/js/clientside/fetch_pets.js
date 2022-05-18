47c8e23c06bf95de07a0f77fb1ecd4ee0c0c4f8c
Purple-Towel <angadgrewal31@gmail.com>
2022-05-18 14:00:19 -0700

    added logic to fill out the pet requests table

--- root/js/clientside/fetch_pets.js
+++ root/js/clientside/fetch_pets.js
@@ -70,6 +70,74 @@ document.addEventListener("DOMContentLoaded", async () => {
         table.appendChild(row);
     }
     
+    let table2 = document.getElementById("caretaker-panel").querySelector("tbody");
+    let requestData = await getRequests();
+    if (requestData.length >= 1) {
+        requestData.forEach( (pet) => {
+            let row = document.createElement("tr");
+            let tableFields = ["photo_url", "name", "species", "gender", "description", "status", "owner_id"];
+    
+            tableFields.forEach( async (field) => {
+                let currentEntry = document.createElement("td");
+                let innerText = "";
+                if (field == "photo_url") {
+                    innerText = `<img src="/img/uploads/${pet[field]}" alt="${pet["name"]}" width="160" height="160">`;
+                } else if (field == "status") {
+                    let state = ""
+                    let allowChange = false;
+                    let status = pet['status'];
+                    if (status == 0) {
+                        state = "Home"
+                        allowChange = true;
+                    } else if (status == 1) {
+                        state = "Away"
+                    } else {
+                        state = "Pending"
+                        allowChange = true;
+                    }
+                    
+                    let button = allowChange ? `<button onclick="changePetState(${pet["id"]})">Change</button>` : `<button disabled>Disabled</button>`
+                    
+                    innerText = 
+                    `
+                        ${state}
+                        <br>
+                        ${button}
+                    `
+                } else if (field == "owner_id") {
+                    if (pet[field] == null) {
+                        innerText = "None";
+                    } else {
+                        let data = await getAccountInfo(pet[field]);
+                        innerText = 
+                        `
+                            ${data[0]["lastname"]}, ${data[0]["firstname"]}
+                            <br>
+                            ${data[0]["username"]} 
+                            <br> 
+                            (${data[0]["email"]})
+                        `;
+                    }
+                } else {
+                    innerText = pet[field];
+                }
+                currentEntry.innerHTML = innerText;
+                row.appendChild(currentEntry);
+            });
+    
+            table2.appendChild(row);
+        })
+    } else {
+        let row = document.createElement("tr");
+        for (let i = 0; i < 7; i++) {
+            let currentEntry = document.createElement("td");
+            let innerText = "N/A";
+            currentEntry.innerHTML = innerText;
+            
+            row.appendChild(currentEntry);
+        }
+        table2.appendChild(row);
+    }
 });
 
 async function getPets() {
