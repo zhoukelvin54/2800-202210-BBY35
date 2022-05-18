6a440bd42cdca3d4d0be9f34d6430967d11da3b6
Purple-Towel <angadgrewal31@gmail.com>
2022-05-18 12:33:14 -0700

    refactored the pets display to show N/A if no pets

--- root/js/clientside/fetch_pets.js
+++ root/js/clientside/fetch_pets.js
@@ -3,61 +3,73 @@
 document.addEventListener("DOMContentLoaded", async () => {
     let table = document.getElementById("pet-table").querySelector("tbody");
     let petData = await getPets();
-
-    petData.forEach( (pet) => {
-        let row = document.createElement("tr");
-        let tableFields = ["photo_url", "name", "species", "gender", "description", "status", "caretaker_id"];
-
-        tableFields.forEach( async (field) => {
-            let currentEntry = document.createElement("td");
-            let innerText = "";
-            if (field == "photo_url") {
-                innerText = `<img src="/img/uploads/${pet[field]}" alt="${pet["name"]}" width="160" height="160">`;
-            } else if (field == "status") {
-                let state = ""
-                let allowChange = false;
-                let status = pet['status'];
-                if (status == 0) {
-                    state = "Home"
-                    allowChange = true;
-                } else if (status == 1) {
-                    state = "Away"
-                } else {
-                    state = "Pending"
-                    allowChange = true;
-                }
-                
-                let button = allowChange ? `<button onclick="changePetState(${pet["id"]})">Change</button>` : `<button disabled>Disabled</button>`
-                
-                innerText = 
-                `
-                    ${state}
-                    <br>
-                    ${button}
-                `
-            } else if (field == "caretaker_id") {
-                if (pet[field] == null) {
-                    innerText = "None";
-                } else {
-                    let data = await getCaretaker(pet[field]);
+    if (petData.length >= 1) {
+        petData.forEach( (pet) => {
+            let row = document.createElement("tr");
+            let tableFields = ["photo_url", "name", "species", "gender", "description", "status", "caretaker_id"];
+    
+            tableFields.forEach( async (field) => {
+                let currentEntry = document.createElement("td");
+                let innerText = "";
+                if (field == "photo_url") {
+                    innerText = `<img src="/img/uploads/${pet[field]}" alt="${pet["name"]}" width="160" height="160">`;
+                } else if (field == "status") {
+                    let state = ""
+                    let allowChange = false;
+                    let status = pet['status'];
+                    if (status == 0) {
+                        state = "Home"
+                        allowChange = true;
+                    } else if (status == 1) {
+                        state = "Away"
+                    } else {
+                        state = "Pending"
+                        allowChange = true;
+                    }
+                    
+                    let button = allowChange ? `<button onclick="changePetState(${pet["id"]})">Change</button>` : `<button disabled>Disabled</button>`
+                    
                     innerText = 
                     `
-                        ${data[0]["lastname"]}, ${data[0]["firstname"]}
+                        ${state}
                         <br>
-                        ${data[0]["username"]} 
-                        <br> 
-                        (${data[0]["email"]})
-                    `;
+                        ${button}
+                    `
+                } else if (field == "caretaker_id") {
+                    if (pet[field] == null) {
+                        innerText = "None";
+                    } else {
+                        let data = await getCaretaker(pet[field]);
+                        innerText = 
+                        `
+                            ${data[0]["lastname"]}, ${data[0]["firstname"]}
+                            <br>
+                            ${data[0]["username"]} 
+                            <br> 
+                            (${data[0]["email"]})
+                        `;
+                    }
+                } else {
+                    innerText = pet[field];
                 }
-            } else {
-                innerText = pet[field];
-            }
+                currentEntry.innerHTML = innerText;
+                row.appendChild(currentEntry);
+            });
+    
+            table.appendChild(row);
+        })
+    } else {
+        let row = document.createElement("tr");
+        for (let i = 0; i < 7; i++) {
+            let currentEntry = document.createElement("td");
+            let innerText = "N/A";
             currentEntry.innerHTML = innerText;
+            
             row.appendChild(currentEntry);
-        });
-
+        }
         table.appendChild(row);
-    })
+    }
+    
 });
 
 async function getPets() {
