cef1e4bb009b3c94a73ca1f7246c91f58da7e69d
Purple-Towel <angadgrewal31@gmail.com>
2022-05-17 18:44:53 -0700

    tweaked the seed pets to now add their status on insertion

--- schemas/setup_database.sql
+++ schemas/setup_database.sql
@@ -149,21 +149,21 @@ email, password, is_admin, is_caretaker) VALUES (
 13, 'user3', 'End', 'User', 'user@email.com', '$2b$10$vAtaahOy4/IHyER30cSC.uqpHHawpIf1kbMNQJTAM73ABVjUr3iMS', 0, 0);
 
 -- Add data to BBY35_pets table
-INSERT INTO BBY35_pets (`id`,`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`) VALUES (1,1,4,'cedric-vt-IuJc2qh2TcA-unsplash.jpeg','Whisker','Cat','F','Meow meow meow');
+INSERT INTO BBY35_pets (`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`, `status`) VALUES (1,4,'cedric-vt-IuJc2qh2TcA-unsplash.jpeg','Whisker','Cat','F','Meow meow meow',1);
 
-INSERT INTO BBY35_pets (`id`,`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`) VALUES (2,1,NULL,'dog_1.jpg','King','Dog','M','Bark bark bark');
+INSERT INTO BBY35_pets (`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`, `status`) VALUES (1,NULL,'dog_1.jpg','King','Dog','M','Bark bark bark',0);
 
-INSERT INTO BBY35_pets (`id`,`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`) VALUES (3,2,3,'cedric-vt-IuJc2qh2TcA-unsplash.jpeg','Whisker','Cat','F','Meow meow meow');
+INSERT INTO BBY35_pets (`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`, `status`) VALUES (2,3,'cedric-vt-IuJc2qh2TcA-unsplash.jpeg','Whisker','Cat','F','Meow meow meow',1);
 
-INSERT INTO BBY35_pets (`id`,`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`) VALUES (4,2,NULL,'dog_1.jpg','King','Dog','M','Bark bark bark');
+INSERT INTO BBY35_pets (`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`, `status`) VALUES (2,NULL,'dog_1.jpg','King','Dog','M','Bark bark bark',0);
 
-INSERT INTO BBY35_pets (`id`,`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`) VALUES (5,3,2,'cedric-vt-IuJc2qh2TcA-unsplash.jpeg','Whisker','Cat','F','Meow meow meow');
+INSERT INTO BBY35_pets (`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`, `status`) VALUES (3,2,'cedric-vt-IuJc2qh2TcA-unsplash.jpeg','Whisker','Cat','F','Meow meow meow',1);
 
-INSERT INTO BBY35_pets (`id`,`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`) VALUES (6,3,NULL,'dog_1.jpg','King','Dog','M','Bark bark bark');
+INSERT INTO BBY35_pets (`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`, `status`) VALUES (3,NULL,'dog_1.jpg','King','Dog','M','Bark bark bark',0);
 
-INSERT INTO BBY35_pets (`id`,`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`) VALUES (7,4,1,'cedric-vt-IuJc2qh2TcA-unsplash.jpeg','Whisker','Cat','F','Meow meow meow');
+INSERT INTO BBY35_pets (`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`, `status`) VALUES (4,1,'cedric-vt-IuJc2qh2TcA-unsplash.jpeg','Whisker','Cat','F','Meow meow meow',1);
 
-INSERT INTO BBY35_pets (`id`,`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`) VALUES (8,4,NULL,'dog_1.jpg','King','Dog','M','Bark bark bark');
+INSERT INTO BBY35_pets (`owner_id`,`caretaker_id`,`photo_url`,`name`,`species`,`gender`,`description`, `status`) VALUES (4,NULL,'dog_1.jpg','King','Dog','M','Bark bark bark',0);
 
 
 -- Add data to BBY35_caretaker_info table
