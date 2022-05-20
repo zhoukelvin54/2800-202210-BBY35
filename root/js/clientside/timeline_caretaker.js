d3a9a91140e631b6b5d04eacd1735f097b5ddc75
Dakaro Mueller <LukarioMC@gmail.com>
2022-05-19 23:40:14 -0700

    Tried working on timelines...

--- root/js/clientside/timeline_caretaker.js
+++ root/js/clientside/timeline_caretaker.js
@@ -1,16 +1,33 @@
+"use strict";
+
 onReady(() => {
   // Do things on load!
+  appendTimelines();
+
+  let cards = document.querySelectorAll(".timeline.card");
 });
 
 async function appendTimelines() {
-  let timelines = await fetch("/timeline/caretaker");
-  
-  for (let i; i < timelines.length; i++) {
-    document.querySelector("main").appendChild(createTimelineCard(timelines[i]));
+  let data = await fetch("/timeline/caretaker").then(async res => {
+    return JSON.parse(await res.text());
+  });
+
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
+    document.querySelector("main").appendChild(createTimelineCard(data[i]));
   }
 }
 
-function createTimelineCard(timeline) {
-  // TODO construct timeline card from page template
-  console.log(timeline);
+async function createTimelineCard(timeline) {
+  let template = document.getElementById("timeline_template");
+  let newTimeline = template.content.cloneNode(true);
+  
+  newTimeline.id = timeline.pet_id;
+  let petData = await fetch(`/petData/${timeline.pet_id}`).then(async res => {
+    return JSON.parse(await res.text());
+  });
+  newTimeline.querySelector(".pet_name").innerText = petData.name;
+  // newTimeline.querySelector(".status").innerText = ""; 
+  
+  return newTimeline
 }
