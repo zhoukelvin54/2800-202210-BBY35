a0d64945ed5f2f8245e89babb870399b086eef30
Dakaro Mueller <LukarioMC@gmail.com>
2022-05-20 00:33:17 -0700

    Got basic timeline data to be displayed. Dates need to be formatted.

--- root/js/clientside/timeline_caretaker.js
+++ root/js/clientside/timeline_caretaker.js
@@ -1,12 +1,19 @@
 "use strict";
 
-onReady(() => {
-  // Do things on load!
-  appendTimelines();
+onReady(async () => {
+  await appendTimelines();
 
   let cards = document.querySelectorAll(".timeline.card");
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", e => {
+      window.location.assign(`/timeline/overview/${e.target.id}`);
+    });
+  }
 });
 
+/**
+ * Fetches the timelines then appends each card for each timeline.
+ */
 async function appendTimelines() {
   let data = await fetch("/timeline/caretaker").then(async res => {
     return JSON.parse(await res.text());
@@ -14,20 +21,34 @@ async function appendTimelines() {
 
   for (let i = 0; i < data.length; i++) {
     console.log(data[i]);
-    document.querySelector("main").appendChild(createTimelineCard(data[i]));
+    document.querySelector("main").appendChild(await createTimelineCard(data[i]));
   }
 }
 
+
+/**
+ * Creates a timeline card and inserts its information
+ * @param { JSON } timeline 
+ * @returns new timeline card
+ */
 async function createTimelineCard(timeline) {
   let template = document.getElementById("timeline_template");
   let newTimeline = template.content.cloneNode(true);
+  let card = newTimeline.firstElementChild;
   
-  newTimeline.id = timeline.pet_id;
-  let petData = await fetch(`/petData/${timeline.pet_id}`).then(async res => {
-    return JSON.parse(await res.text());
+  card.id = timeline.timeline_id;
+  let petData = await fetch(`/getPetInfo/${timeline.pet_id}`).then(async res => {
+    return JSON.parse(await res.text())[0];
   });
-  newTimeline.querySelector(".pet_name").innerText = petData.name;
-  // newTimeline.querySelector(".status").innerText = ""; 
-  
-  return newTimeline
+  let status = petData.status == 2 ? "In Queue" : petData.status == 1 ? "Away" : "Home"
+
+  card.querySelector(".pet_name").innerText = petData.name;
+  card.querySelector(".status").innerText = status; 
+  let dates = card.querySelectorAll(".timeline_date");
+  if (dates.length > 0) {
+    dates[0].innerText = timeline.start_date;
+    dates[1].innerText = timeline.end_date;
+  }
+
+  return newTimeline;
 }
