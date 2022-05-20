24df263ff4ef294a531b70d5b1589c70e433d808
Kelvin Zhou <szhou54bcit@gmail.com>
2022-05-20 14:04:45 -0700

    added js for audio easter egg

--- root/js/clientside/easterEgg.js
+++ root/js/clientside/easterEgg.js
@@ -1,3 +1,20 @@
 function myFunction() {
     document.getElementById("easterJS").innerHTML = "Team Members:<br>Jashanjot Singh<br>Angad Grewal<br>Dakaro Mueller<br>Kelvin Zhou";
 }
+var audio;
+var clicks = 0;
+
+document.getElementById("logo").addEventListener("click", function () {
+    clicks += 1;
+    if (clicks == 3) {
+        var chosenValue = Math.random() < 0.5 ? 1 : 2;
+        if (chosenValue == 1) {
+            audio = new Audio('audio/catsounds.mp3');
+            audio.play();
+        } else {
+            audio = new Audio('audio/dogsounds.mp3');
+            audio.play();
+        }    
+        clicks = 0;
+    }
+});
